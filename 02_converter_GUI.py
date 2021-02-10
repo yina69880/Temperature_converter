fc3a8ac5768fc777cbed30cec3a33d6195653866
yina69880 <60630442+yina69880@users.noreply.github.com>
2021-02-10 13:08:00 +1300

    Create 02_converter_GUI.py

---
+++ 02_converter_GUI.py
@@ -0,0 +1,49 @@
+from tkinter import *
+from functools import partial   # To prevent unwanted windows
+
+import random
+
+
+class Converter:
+    def __init__(self):
+
+        # formatting vaiables...
+        background_color = "light blue"
+
+        # converter frame
+        self.converter_frame = Frame(width=300, bg=background_color,
+                                     pady=10)
+        self.converter_frame.grid()
+        # temperature converter heading (row 0)
+        self.temp_heading_label = Label(self.converter_frame,
+                                        text="Temperature Converter",
+                                        font="Arial 16 bold",
+                                        bg=background_color,
+                                        padx=10, pady=10)
+        self.temp_heading_label.grid(row=0)
+        # User instructions (row 1)
+
+        self.temp_instructions_label = Label(self.converter_frame,
+                                             text="Type in the amount to be "
+                                                  "converted and then put "
+                                                  "one of the buttons below...",
+                                        font="Arial 10 italic", wrap=250,
+                                        justify=LEFT, bg=background_color,
+                                        padx=10, pady=10)
+        self.temp_instructions_label.grid(row=1)
+
+        # temperature entry box (row 2)
+        self.to_convert_entry = Entry(self.converter_frame, width=20,
+                                      font="Arial 14 bold")
+        self.to_convert_entry.grid(row=2)
+
+        # Converion button frame (row 3)
+
+        # answer label row
+
+# main routine
+if __name__== "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    something = Converter()
+    root.mainloop()
