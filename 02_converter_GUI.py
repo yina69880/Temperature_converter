2b7b627be396713f1eeb9a6c98ccc8230587a001
yina69880 <60630442+yina69880@users.noreply.github.com>
2021-02-12 10:54:54 +1300

    too cold!!

--- 02_converter_GUI.py
+++ 02_converter_GUI.py
@@ -43,17 +43,20 @@ class Converter:
 
         self.to_c_button = Button(self.conversion_buttons_frame,
                                   text="To Centidgrade", font="Arial 10 bold",
-                                  bg="Khaki1", padx=10, pady=10)
+                                  bg="Khaki1", padx=10, pady=10,
+                                  command=lambda: self.temp_convert(-459))
         self.to_c_button.grid(row=0, column=0)
 
         self.to_f_button = Button(self.conversion_buttons_frame,
                                   text="To Farenheit", font="Arial 10 bold",
-                                  bg="Orchid1", padx=10, pady=10)
+                                  bg="Orchid1", padx=10, pady=10,
+                                  command=lambda: self.temp_convert(-273))
         self.to_f_button.grid(row=0, column=1)
 
         # answer label row
         self.converted_label = Label(self.converter_frame, font="Arial 14 bold",
-                                     fg="purple", bg=background_color, pady=10)
+                                     fg="purple", bg=background_color, pady=10,
+                                     text="Conversion goes here")
         self.converted_label.grid(row=4)
 
         # History / Help button frame (row 5)
@@ -67,6 +70,59 @@ class Converter:
         self.help_button = Button(self.hist_help_frame, font="Arial 12 bold",
                                         text="Help", width=5)
         self.help_button.grid(row=0, column=1)
+
+    def temp_convert(self, low):
+        print(low)
+
+        error = "#ffafaf" # pale pink background for when entry box has errors
+
+        # retrieve amount entered into entry field
+        to_convert = self.to_convert_entry.get()
+
+        try:
+            to_convert = float(to_convert)
+            has_errors = "no"
+
+            # check amount is a valid number
+
+            # convert to F
+            if low == -273 and to_convert >= low:
+                farenheit = (to_convert * 9 / 5) + 32
+                to_convert = self.round_it(to_convert)
+                farenheit = self.round_it(farenheit)
+                answer = "{} degrees C {} degrees F".format(to_convert, farenheit)
+            # convert to C
+            elif low == -459 and to_convert >= low:
+                celsius = (to_convert - 32) * 5/9
+                to_convert = self.round_it(to_convert)
+                celsius = self.round_it(celsius)
+                answer = "{} degrees C {} degrees F".format(to_convert, celsius)
+
+            else:
+                # input too cold
+                answer = "Too Cold!!"
+                has_errors = "yes"
+
+            # display answer
+            if has_errors == "no":
+                self.converted_label.configure(text=answer, fg="blue")
+                self.to_convert_entry.configure(bg="white")
+            else:
+                self.converted_label.configure(text=answer, fg="red")
+                self.to_convert_entry.configure(bg=error)
+
+            # add answer to list for history
+
+        except ValueError:
+            self.converted_label.configure(text="Enter a number!!", fg="red")
+            self.to_convert_entry.configure(bg=error)
+
+    def round_it(self, to_round):
+        if to_round % 1 == 0:
+            rounded = int(to_round)
+        else:
+            rounded = round(to_round, 1)
+
 
 # main routine
 if __name__== "__main__":
