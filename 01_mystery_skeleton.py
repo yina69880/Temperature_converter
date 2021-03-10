f8f665b5f325441010c1508ff3ec7f6ff758db12
yina69880 <60630442+yina69880@users.noreply.github.com>
2021-03-10 13:22:58 +1300

    started mystery box

---
+++ 01_mystery_skeleton.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from functools import partial # To prevent unwanted windows
+import random
+
+
+class Start:
+    def __init__(self, parent):
+
+        # GUI to get starting balance and stakes
+        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame.grid()
+
+        # Mystery Heading (row 0)
+        self.mystery_box_label = Label(self.start_frame, text="Mystery Box Game",
+                                       font="Arial 19 bold")
+        self.mystery_box_label.grid(row=1)
+
+        # Entry box... (row 1)
+        
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# Temperature_converter
- com301
